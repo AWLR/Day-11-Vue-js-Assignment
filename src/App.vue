7a7d3169a3d40317737fd120be4e9c9e0313f134
AWLR <lehrelex@gmail.com>
2024-01-12 16:52:57 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container">
+  <div id="counter">
+      <h1>Vue Js Assignment</h1>
+      <p>The Current Count : {{ count }}</p>
+     
+      <button v-on:click="increment">Increment</button>
+      <button v-on:click="decrement">Decrement</button>
+      <h2> {{msg}}</h2>
+  </div>
+</div>
+</template>
+
+<script>
+export default{
+
+  data() {
+      return {
+          count: 0,
+           msg:null,
+
+      };
+  },
+  methods: {
+      increment() {
+          this.count++;
+          this.msg= "Counter Increasing";
+      },
+      decrement() {
+          this.count--;
+          this.msg= "Counter Decreasing";
+      }
+  
+
+      },
+  }
+
+</script>
+
+<style>
+*{
+  padding: 10px;
+  justify-content: center;
+  text-align: center;
+}
+.container{
+  justify-content: center;
+}
+
+#counter{
+  border: 3px solid greenyellow;
+}
+
+button {
+background-color: #040aaa; 
+margin-right: 5px;
+border: none;
+color: white;
+font-size: 15px;
+}
+p{
+  font-size:larger;
+  color: red;
+}
+</style>
+
+
